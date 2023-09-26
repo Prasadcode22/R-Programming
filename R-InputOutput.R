59bc6c5a1ac86eca1769aac21edf61ec40f6a5b5
Prasad Aher <aherprasad1212@gmail.com>
2023-09-26 21:05:13 +0530

    add files

---
+++ R-InputOutput.R
@@ -0,0 +1,18 @@
+# <----------------------------------------<<< INPUT/OUTPUT IN R PROGRAMMING>>>----------------------------------------------------> #nolint
+# METHODS
+# <---------------------------------------------<<< READLINE FUNCTION >>>----------------------------------------------------------> #nolint
+
+# In R language readline() method takes input in string format. If one inputs an integer then it is inputted as a string, lets say, #nolint
+# one wants to input 255, then it will input as “255”, like a string. So one needs to convert that inputted value to the format that #nolint 
+# he needs. In this case, string “255” is converted to integer 255. To convert the inputted value to the desired data type, there are #nolint
+# some functions in R, #nolint
+
+
+
+
+# <-----------------------------------------------<<< SCAN FUNCTION >>>------------------------------------------------------------> #nolint
+
+
+
+
+# <-----------------------------------------------<<< THE END >>>------------------------------------------------------------------> #nolint
